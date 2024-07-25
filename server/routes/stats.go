f97abf10c88eef36a075234306948d3ad489cca0
jarox <georgehd03@gmail.com>
2024-07-25 11:15:05 +0200

    Added cache param for user to choose if they want to use the cached value or recalculate it

--- server/routes/stats.go
+++ server/routes/stats.go
@@ -3,31 +3,48 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/jaroxcraft/vps-dash-server/logger"
 	"github.com/jaroxcraft/vps-dash-server/security"
 	"github.com/jaroxcraft/vps-dash-server/system"
 )
 
-func GetCPU(server *http.ServeMux) {
-	type Response struct {
-		Usage uint8 `json:"usage"`
-	}
+type Response struct {
+	Usage uint8 `json:"usage"`
+}
 
-	server.HandleFunc("GET /stats/cpu/", func(w http.ResponseWriter, r *http.Request) {
+func usageHandler(server *http.ServeMux, path string, getCachedUsage func() uint8, getUsage func() uint8) {
+	server.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
-		allowed := security.ValidateRequest(*r)
-		if !allowed {
+		if !security.ValidateRequest(*r) {
 			http.Error(w, "Token invalid", http.StatusUnauthorized)
 
 			return
 		}
 
-		response := Response{system.GetCachedCPUUsage()}
+		response := Response{}
 
-		err := json.NewEncoder(w).Encode(response)
+		cacheParam := r.URL.Query().Get("cache")
+		if cacheParam == "" {
+			cacheParam = "true"
+		}
+
+		cache, err := strconv.ParseBool(cacheParam)
 		if err != nil {
+			http.Error(w, "Cache param must be a boolean", http.StatusBadRequest)
+
+			return
+		}
+
+		if cache {
+			response.Usage = getCachedUsage()
+		} else {
+			response.Usage = getUsage()
+		}
+
+		if err := json.NewEncoder(w).Encode(response); err != nil {
 			logger.Get().Warnf("Failed encoding response: %v", err)
 
 			return
@@ -35,28 +52,10 @@ func GetCPU(server *http.ServeMux) {
 	})
 }
 
+func GetCPU(server *http.ServeMux) {
+	usageHandler(server, "GET /stats/cpu/", system.GetCachedCPUUsage, system.GetCPUUsage)
+}
+
 func GetMemory(server *http.ServeMux) {
-	type Response struct {
-		Usage uint8 `json:"usage"`
-	}
-
-	server.HandleFunc("GET /stats/memory/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-		allowed := security.ValidateRequest(*r)
-		if !allowed {
-			http.Error(w, "Token invalid", http.StatusUnauthorized)
-
-			return
-		}
-
-		response := Response{system.GetCachedMemoryUsage()}
-
-		err := json.NewEncoder(w).Encode(response)
-		if err != nil {
-			logger.Get().Warnf("Failed encoding response: %v", err)
-
-			return
-		}
-	})
+	usageHandler(server, "GET /stats/memory/", system.GetCachedMemoryUsage, system.GetMemoryUsage)
 }
